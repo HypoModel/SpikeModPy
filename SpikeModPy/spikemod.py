c64ba26927a48081b19783e44956d79143cbf146
Duncan MacGregor <doctorafternoon@gmail.com>
2026-03-20 12:24:32 +0800

    CMML3 merge cleanup

--- SpikeModPy/spikemod.py
+++ SpikeModPy/spikemod.py
@@ -2,13 +2,20 @@
 
 import wx
 import random
-import numpy as np
+import os
+import math
+from datetime import datetime
 
-from HypoModPy.hypomods import *
-from HypoModPy.hypoparams import *
-from HypoModPy.hypodat import *
-from HypoModPy.hypogrid import *
-from HypoModPy.hypospikes import *
+
+from HypoModPy.hypomods import (
+    Mod, ModThread, ModThreadEvent,
+    ModThreadCompleteEvent, ModThreadProgressEvent,
+    DiagWrite
+)
+from HypoModPy.hypoparams import ParamBox
+from HypoModPy.hypodat import PlotDat, pdata
+from HypoModPy.hypogrid import GridBox
+from HypoModPy.hypospikes import SpikeDat, SpikeDataBox
 
 from spikepanels import SpikeBox, SecBox
 
